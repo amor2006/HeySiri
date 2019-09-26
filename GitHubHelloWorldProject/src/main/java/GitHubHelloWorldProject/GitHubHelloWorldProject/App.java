4721a4c4c7eeacd9eb4ff1a70def2d7edf3b8a70
amor2006 <36248164+amor2006@users.noreply.github.com>
2019-09-25 21:21:29 -0400

    Add files via upload

---
+++ GitHubHelloWorldProject/src/main/java/GitHubHelloWorldProject/GitHubHelloWorldProject/App.java
@@ -0,0 +1,13 @@
+package GitHubHelloWorldProject.GitHubHelloWorldProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
